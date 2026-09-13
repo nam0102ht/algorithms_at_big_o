3c5432c37fe9eb8e84df4631721281a3b5010c77
namnguyen <namnha@gosoft.co.th>
2026-09-13 20:00:25 +0700

    add code of max distinct for sliding window

---
+++ src/main/java/com/ntnn/amazon/CountDistinctElementsinEveryWindowofSizeK.java
@@ -0,0 +1,50 @@
+package com.ntnn.amazon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountDistinctElementsinEveryWindowofSizeK {
+  /*
+  * Let’s switch to another important pattern: Sliding Window.
+  * Amazon analyzes a stream of product IDs. Given an integer array products and an integer k, find the maximum number of distinct products in any contiguous window of size k.
+  * Example:
+  * products = [1, 2, 1, 3, 4, 2, 3]
+  * k = 4
+  * [1, 2, 1, 3] → 3 distinct
+  * [2, 1, 3, 4] → 4 distinct
+  * [1, 3, 4, 2] → 4 distinct
+  * [3, 4, 2, 3] → 3 distinct
+  * */
+
+  public static void main(String[] args) {
+    int[] products = new int[] {1, 2, 1, 3, 4, 2, 3};
+    int k = 4;
+    System.out.println("Max distinct products: " + maxDistinctProducts(products, k));
+  }
+
+
+  public static int maxDistinctProducts(int[] products, int k) {
+    int left = 0;
+    int n = products.length;
+    int maxDistinct = 0;
+    Map<Integer, Integer> frequency = new HashMap<>();
+    for (int right = 0; right < n; right++) {
+      int incoming = products[right];
+      frequency.put(incoming, frequency.getOrDefault(right, 0) + 1);
+      if (right - left + 1 > k) {
+        int outgoing = products[left];
+        frequency.put(outgoing, frequency.get(outgoing) - 1);
+        if (frequency.get(outgoing) == 0) {
+          frequency.remove(outgoing);
+        }
+        left++;
+
+      }
+
+      if (right - left + 1 == k) {
+        maxDistinct = Math.max(maxDistinct, frequency.size());
+      }
+    }
+    return maxDistinct;
+  }
+}
